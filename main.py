81cbd9ea6f2e3a94fb03642c85555d37a978d2a9
Daley Mottley <daley.mottley@hotmail.com>
2024-08-17 07:05:22 +0000

    Assign the PORT to 5000

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def recipe():
     return render_template('recipe.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
