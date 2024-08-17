b91b38aa552515557bc352a4e00e9d1ea313383f
Daley Mottley <daley.mottley@hotmail.com>
2024-08-17 15:43:46 +0000

    Adjust main.py to handle services, logs and routes.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes.routes import routes
+from services.logging_service import setup_logging
+from config import DevelopmentConfig, ProductionConfig
+import os
 
+# Initialize logging
+setup_logging()
+
+# Initialize Flask app
 app = Flask(__name__)
 
+# Load configuration based on the environment
+if os.getenv('FLASK_ENV') == 'production':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(DevelopmentConfig)
 
-@app.route('/')
-def recipe():
-    return render_template('recipe.html')
-
+# Register routes and error handlers
+app.register_blueprint(routes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
